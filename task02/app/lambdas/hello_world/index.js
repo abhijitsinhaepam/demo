c93461339cca9e0f28fb5996725b6dd55b9c3a12
Abhijit Sinha <abhijit_sinha@epam.com>
2025-03-07 02:31:37 +0530

    fixxxx113

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,17 +1,36 @@
+// exports.handler = async (event) => {
+//     let response;
+//     if (event.rawPath === "/cmtr-41cac6d3" && event.httpMethod === "GET") {
+//         response = {
+//             statusCode: 400,
+//             body: JSON.stringify({
+//                 message: "Bad request syntax or unsupported method. Request path: /cmtr-41cac6d3. HTTP method: GET"
+//             }),
+//         };
+//     } else {
+//         response = {
+//             statusCode: 200,
+//             body: JSON.stringify({ message: "Hello from Lambda" }),
+//         };
+//     }
+//     return response;
+// };
+
 exports.handler = async (event) => {
     let response;
+    
     if (event.rawPath === "/cmtr-41cac6d3" && event.httpMethod === "GET") {
         response = {
             statusCode: 400,
-            body: JSON.stringify({
-                message: "Bad request syntax or unsupported method. Request path: /cmtr-41cac6d3. HTTP method: GET"
-            }),
+            message: "Bad request syntax or unsupported method. Request path: /cmtr-41cac6d3. HTTP method: GET"
         };
     } else {
         response = {
             statusCode: 200,
-            body: JSON.stringify({ message: "Hello from Lambda" }),
+            message: "Hello from Lambda"
         };
     }
+    
     return response;
 };
+
